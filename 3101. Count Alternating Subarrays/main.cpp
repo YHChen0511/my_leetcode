455ec844667c3c60e7ff81aa9d35db8da92d8995
YHChen0511 <1009283848@sjtu.edu.cn>
2024-12-30 17:26:10 +0800

    2734

--- 3101. Count Alternating Subarrays/main.cpp
+++ 3101. Count Alternating Subarrays/main.cpp
@@ -2,20 +2,25 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    long long countAlternatingSubarrays(vector<int>& nums) {
+    long long countAlternatingSubarrays(vector<int> &nums)
+    {
         int n = nums.size();
         long long ans = 1, cnt = 1;
-        for(int i = 1; i < n; i++){
-            if(nums[i] != nums[i-1]){
+        for (int i = 1; i < n; i++)
+        {
+            if (nums[i] != nums[i - 1])
+            {
                 cnt += 1;
-            } else{
+            }
+            else
+            {
                 cnt = 1;
             }
             ans += cnt;
         }
-
 
         return ans;
     }
